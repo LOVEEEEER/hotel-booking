8b0294028e3d56699bff3e54bfdbab88d25f57ff
Nikita Demyanenko <nikitademyanenko000@gmail.com>
2022-09-06 17:32:24 +0300

    add sign in page

--- src/app/App.js
+++ src/app/App.js
@@ -2,11 +2,13 @@ import { Route, Switch } from "react-router-dom";
 import Main from "./layouts/Main";
 import Rooms from "./layouts/Rooms";
 import Error from "./layouts/Error";
+import Login from "./layouts/Login";
 
 function App() {
   return (
     <>
       <Switch>
+        <Route path="/login/signin" component={Login} />
         <Route path="/rooms/:roomId?" component={Rooms} />
         <Route path="/" exact component={Main} />
         <Route component={Error} />
---
+++ src/app/layouts/Login.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../components/common/Header";
+import SignInPage from "../components/page/SignInPage";
+
+const Login = () => {
+  return (
+    <>
+      <Header />
+      <SignInPage />
+    </>
+  );
+};
+
+export default Login;
