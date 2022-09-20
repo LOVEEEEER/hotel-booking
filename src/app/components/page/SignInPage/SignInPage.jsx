95d48ea0eacb06e49e5d8adf6c88fbc60c05efae
Nikita Demyanenko <nikitademyanenko000@gmail.com>
2022-09-20 16:27:01 +0300

    add some foldres

--- src/app/components/page/SignInPage/SignInPage.jsx
+++ src/app/components/page/SignInPage/SignInPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import SignInForm from "../../ui/SignInForm";
+import SignInForm from "../../ui/forms/SignInForm";
 import { Link } from "react-router-dom";
 
 const SignInPage = () => {
