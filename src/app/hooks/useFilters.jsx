08108647a844d32408089322ce63e4a4a87e532e
Nikita Demyanenko <nikitademyanenko000@gmail.com>
2022-10-18 05:50:08 +0300

    fix bag with eslints

--- src/app/hooks/useFilters.jsx
+++ src/app/hooks/useFilters.jsx
@@ -7,57 +7,56 @@ export const useFilters = () => {
         setSearchQuery(target.value);
     };
 
-    function isRepeatElement(items, item) {
-        return items.some((_item) => _item.id === item.id);
-    }
+    // function isRepeatElement(items, item) {
+    //     return items.some((_item) => _item.id === item.id);
+    // }
 
     const getFilteredItems = (items, filtersValue) => {
-        const filteredItems = [];
-        const filters = Object.keys(filtersValue).filter((item) => {
-            return filtersValue[item]?.length ? item : null;
-        });
+        // const filteredItems = [];
+        // const filters = Object.keys(filtersValue).filter((item) => {
+        //     return filtersValue[item]?.length ? item : null;
+        // });
         if (items) {
-            filters.forEach((item) => {
-                switch (item) {
-                    case "comfort": {
-                        // items.forEach((room) => {
-                        //     filtersValue.comfort.forEach((value) => {
-                        //         if (room.comfort.includes(value)) {
-                        //             const isRepeat = isRepeatElement(
-                        //                 filteredItems,
-                        //                 room
-                        //             );
-                        //             if (!isRepeat) {
-                        //                 filteredItems.push(room);
-                        //             }
-                        //         }
-                        //     });
-                        // });
-                        // items.forEach(room=>{
-                        //    room.comfort(item=>)
-                        // })
-                        // items.forEach(room=>{
-                        //     fi
-                        // })
-                    }
-                    case "rate": {
-                        console.log(filteredItems);
-                        items.forEach((room) => {
-                            if (room.rate.toString() === filtersValue.rate) {
-                                const isRepeat = isRepeatElement(
-                                    filteredItems,
-                                    room
-                                );
-                                if (!isRepeat) {
-                                    filteredItems.push(room);
-                                }
-                            }
-                        });
-                    }
-                }
-            });
-
-            return filteredItems.length !== 0 ? filteredItems : items;
+            // filters.forEach((item) => {
+            // switch (item) {
+            //     case "comfort": {
+            //         // items.forEach((room) => {
+            //         //     filtersValue.comfort.forEach((value) => {
+            //         //         if (room.comfort.includes(value)) {
+            //         //             const isRepeat = isRepeatElement(
+            //         //                 filteredItems,
+            //         //                 room
+            //         //             );
+            //         //             if (!isRepeat) {
+            //         //                 filteredItems.push(room);
+            //         //             }
+            //         //         }
+            //         //     });
+            //         // });
+            //         // items.forEach(room=>{
+            //         //    room.comfort(item=>)
+            //         // })
+            //         // items.forEach(room=>{
+            //         //     fi
+            //         // })
+            //     }
+            //     case "rate": {
+            //         console.log(filteredItems);
+            //         items.forEach((room) => {
+            //             if (room.rate.toString() === filtersValue.rate) {
+            //                 const isRepeat = isRepeatElement(
+            //                     filteredItems,
+            //                     room
+            //                 );
+            //                 if (!isRepeat) {
+            //                     filteredItems.push(room);
+            //                 }
+            //             }
+            //         });
+            //     }
+            // }
+            // });
+            // return filteredItems.length !== 0 ? filteredItems : items;
         }
     };
 
