4c874ece0a43c342614eaf673fbc08544d85e67b
Nikita Demyanenko <nikitademyanenko000@gmail.com>
2022-11-22 16:42:47 +0300

    add dialog window before delete booking

--- src/app/components/common/Dialog/Dialog.jsx
+++ src/app/components/common/Dialog/Dialog.jsx
@@ -1,11 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-// import Button from "@mui/material/Button";
 import DialogMUI from "@mui/material/Dialog";
-// import DialogActions from "@mui/material/DialogActions";
-// import DialogContent from "@mui/material/DialogContent";
-// import DialogContentText from "@mui/material/DialogContentText";
-// import DialogTitle from "@mui/material/DialogTitle";
 
 const Dialog = ({ open, onClose, children }) => {
     return (
@@ -16,20 +11,6 @@ const Dialog = ({ open, onClose, children }) => {
                 aria-labelledby="alert-dialog-title"
                 aria-describedby="alert-dialog-description"
             >
-                {/* <DialogTitle id="alert-dialog-title">
-                    Номер успешно забронирован!
-                </DialogTitle>
-                <DialogContent>
-                    <DialogContentText id="alert-dialog-description">
-                        Управлять забронированными отелями вы можете в разделе
-                        Мои бронирования
-                    </DialogContentText>
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={onClose} autoFocus>
-                        Agree
-                    </Button>
-                </DialogActions> */}
                 {children}
             </DialogMUI>
         </div>
