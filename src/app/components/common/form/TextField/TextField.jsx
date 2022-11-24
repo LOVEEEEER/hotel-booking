80b7dde1b0c179a6b0b86b386cbbf94d5ca8c2c2
Nikita Demyanenko <nikitademyanenko000@gmail.com>
2022-11-24 08:50:37 +0300

    add password field in sign-in and sign-up form

--- src/app/components/common/form/TextField/TextField.jsx
+++ src/app/components/common/form/TextField/TextField.jsx
@@ -1,20 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import TextFieldMui from "@mui/material/TextField";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import IconButton from "@mui/material/IconButton";
+import FormControl from "@mui/material/FormControl";
+import InputAdornment from "@mui/material/InputAdornment";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputLabel from "@mui/material/InputLabel";
 
 const TextField = ({ name, label, onChange, value, type, ...rest }) => {
+    const [showPassword, setShowPassword] = useState(false);
+    const handleShowPassword = () => {
+        setShowPassword((prevState) => !prevState);
+    };
     return (
-        <TextFieldMui
-            onChange={onChange}
-            name={name}
-            id={name}
-            label={label}
-            variant="outlined"
-            value={value}
-            className="muiinput"
-            type={type}
-            {...rest}
-        />
+        <>
+            {type === "password" ? (
+                <FormControl sx={{ width: "25ch" }} variant="outlined">
+                    <InputLabel htmlFor="outlined-adornment-password">
+                        {label}
+                    </InputLabel>
+                    <OutlinedInput
+                        name={name}
+                        id="outlined-adornment-password"
+                        type={showPassword ? "text" : "password"}
+                        value={value}
+                        onChange={onChange}
+                        {...rest}
+                        endAdornment={
+                            <InputAdornment position="end">
+                                <IconButton
+                                    aria-label="toggle password visibility"
+                                    onClick={handleShowPassword}
+                                    onMouseDown={handleShowPassword}
+                                    edge="end"
+                                >
+                                    {showPassword ? (
+                                        <VisibilityOff />
+                                    ) : (
+                                        <Visibility />
+                                    )}
+                                </IconButton>
+                            </InputAdornment>
+                        }
+                        label="Password"
+                    />
+                </FormControl>
+            ) : (
+                <TextFieldMui
+                    onChange={onChange}
+                    name={name}
+                    id={name}
+                    label={label}
+                    variant="outlined"
+                    value={value}
+                    className="muiinput"
+                    type={type}
+                    {...rest}
+                />
+            )}
+        </>
     );
 };
 
