ed109aa7063eba01fc67834951a3a9f4e7b19d3b
Nikita Demyanenko <nikitademyanenko000@gmail.com>
2022-11-28 17:10:44 +0300

    add layout for favourites page

---
+++ src/app/components/page/FavoritesPage/FavoritesPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FavoritesPage = () => {
+    return (
+        <main className="favorites__page">
+            <h1>Favorites</h1>
+        </main>
+    );
+};
+
+export default FavoritesPage;
