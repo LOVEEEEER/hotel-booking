a1a7a705284159586024fe1792e92359d5541762
Nikita Demyanenko <nikitademyanenko000@gmail.com>
2022-11-29 09:44:06 +0300

    delivered favorite button in common component, add favorites service

--- src/app/components/page/FavoritesPage/FavoritesPage.jsx
+++ src/app/components/page/FavoritesPage/FavoritesPage.jsx
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { loadFavorites } from "../../../store/favorites";
 
 const FavoritesPage = () => {
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(loadFavorites());
+    }, []);
     return (
         <main className="favorites__page">
             <h1>Favorites</h1>
