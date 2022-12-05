f5a763ca036fe5daccffeb07a1409b8df4c348f6
Nikita Demyanenko <nikitademyanenko000@gmail.com>
2022-12-05 18:09:23 +0300

    add tabs in common components

--- src/app/components/page/AdminPage/AdminPage.jsx
+++ src/app/components/page/AdminPage/AdminPage.jsx
@@ -1,42 +1,22 @@
-import React, { useState } from "react";
-import Tabs from "@mui/material/Tabs";
-import Tab from "@mui/material/Tab";
-import Box from "@mui/material/Box";
+import React from "react";
+import Tabs from "../../common/Tabs";
 import UsersTable from "../../ui/UsersTable/UsersTable";
-import TabPanel from "../../common/TabPanel";
 import UsersBookingList from "../../ui/admin/UsersBookingList";
 
 const AdminPage = () => {
-    const [tapPage, setTapPage] = useState(0);
-
-    const handleChange = (page) => {
-        setTapPage(page);
-    };
-
+    const tabsColumns = [
+        {
+            name: "Бронирование",
+            component: <UsersBookingList />
+        },
+        {
+            name: "Пользователи",
+            component: <UsersTable />
+        }
+    ];
     return (
         <main className="admin__page">
-            <Box
-                sx={{ borderBottom: 1, borderColor: "divider", width: "100%" }}
-            >
-                <Tabs
-                    value={tapPage}
-                    onChange={(e, page) => handleChange(page)}
-                    aria-label="basic tabs example"
-                >
-                    <Tab label="Бронирования" />
-                    <Tab label="Пользователи" />
-                    <Tab label="Номера" />
-                </Tabs>
-            </Box>
-            <TabPanel value={tapPage} index={0}>
-                <UsersBookingList />
-            </TabPanel>
-            <TabPanel value={tapPage} index={1}>
-                <UsersTable />
-            </TabPanel>
-            <TabPanel value={tapPage} index={2}>
-                Item Three
-            </TabPanel>
+            <Tabs options={tabsColumns} />
         </main>
     );
 };
