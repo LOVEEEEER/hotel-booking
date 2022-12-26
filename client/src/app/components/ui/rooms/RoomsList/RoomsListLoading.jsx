1af7c4c1233808f84edd4a0c1e9e24664dd4eb17
Nikita Demyanenko <nikitademyanenko000@gmail.com>
2022-12-26 07:23:09 +0300

    add production styles bags

--- client/src/app/components/ui/rooms/RoomsList/RoomsListLoading.jsx
+++ client/src/app/components/ui/rooms/RoomsList/RoomsListLoading.jsx
@@ -7,56 +7,58 @@ const RoomsListLoading = ({ pageSize }) => {
     const array = _.range(0, pageSize - 1);
     return (
         <>
-            <div className="container rooms-list">
-                {array.map((item) => (
-                    <div className="rooms-card-loading" key={item}>
-                        <div className="rooms-card-loading__first-section">
-                            <div className="rooms-card-loading__img">
-                                <Skeleton
-                                    variant="text"
-                                    animation="wave"
-                                    height={391}
-                                    width={391}
-                                />
+            {window.screen.width > 768 && (
+                <div className="container rooms-list">
+                    {array.map((item) => (
+                        <div className="rooms-card-loading" key={item}>
+                            <div className="rooms-card-loading__first-section">
+                                <div className="rooms-card-loading__img">
+                                    <Skeleton
+                                        variant="text"
+                                        animation="wave"
+                                        height={391}
+                                        width={391}
+                                    />
+                                </div>
+                                <div className="rooms-card-loading__first-section_text">
+                                    <Skeleton
+                                        variant="text"
+                                        animation="wave"
+                                        height={50}
+                                        width={391}
+                                    />
+                                    <Skeleton
+                                        variant="text"
+                                        animation="wave"
+                                        height={140}
+                                        width={391}
+                                    />
+                                </div>
                             </div>
-                            <div className="rooms-card-loading__first-section_text">
+                            <div className="rooms-card-loading__second-section">
                                 <Skeleton
                                     variant="text"
                                     animation="wave"
                                     height={50}
-                                    width={391}
+                                    width={90}
                                 />
                                 <Skeleton
                                     variant="text"
                                     animation="wave"
-                                    height={140}
-                                    width={391}
+                                    height={100}
+                                    width={120}
+                                />
+                                <Skeleton
+                                    variant="text"
+                                    animation="wave"
+                                    height={60}
+                                    width={140}
                                 />
                             </div>
                         </div>
-                        <div className="rooms-card-loading__second-section">
-                            <Skeleton
-                                variant="text"
-                                animation="wave"
-                                height={50}
-                                width={90}
-                            />
-                            <Skeleton
-                                variant="text"
-                                animation="wave"
-                                height={100}
-                                width={120}
-                            />
-                            <Skeleton
-                                variant="text"
-                                animation="wave"
-                                height={60}
-                                width={140}
-                            />
-                        </div>
-                    </div>
-                ))}
-            </div>
+                    ))}
+                </div>
+            )}
         </>
     );
 };
